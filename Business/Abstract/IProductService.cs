ea4ec286d9edb6561e646a97f78be33172d963d5
cengizsen <cengizsen98@icloud.com>
2022-07-29 15:53:04 +0300

    ..

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,12 @@ namespace Business.Abstract
 {
    public interface IProductService
     {
-        List<Product> GetAll();//Bütün datayı getirir
-        List<Product> GetAllByCategoryId(int id);//Category datasını verilen category Idsine göre getirir   
-        List<Product> GetByUnitPrice(decimal min, decimal max);//Belirtilen fiyat aralığındaki verileri getirir.
-        List<ProductDetailDto> GetProductDetails();
+        
+        IDataResult<List<Product>> GetAll();//Bütün datayı getirir
+        IDataResult< List<Product>> GetAllByCategoryId(int id);//Category datasını verilen category Idsine göre getirir   
+        IDataResult <List<Product>> GetByUnitPrice(decimal min, decimal max);//Belirtilen fiyat aralığındaki verileri getirir.
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IDataResult <Product> GetById(int productId);
+        IResult Add(Product product);
      }
 }
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+   public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProductNameInValid = "Ürün İsmi Geçersiz";
+        public static string MaintenanceTime = "Sistem Kapalı";
+        public static string ProductsListed = "Ürünler Listelendi";
+
+
+    }
+}
